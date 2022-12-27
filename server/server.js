e01446084deceb3c43c453efa474e8abd722b66c
manikandanb975121 <36506866+manikandanb975121@users.noreply.github.com>
2022-12-28 00:23:46 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: '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'
+});
+
+console.log('key', process.env.OPENAI_API_KEY)
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res, next) => {
+    res.status(200).send({
+        message: 'Hello from Codex'
+    })
+})
+
+app.post('/', async(req, res) => {
+    try {
+        console.log('req')
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            // prompt: "\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+
+        console.log(response.data);
+        res.status(200).json({
+            bot: response.data.choices[0].text
+        })
+    } catch(err) {
+        console.log({err});
+        res.status(500).send({error})
+    }
+})
+
+app.listen(5000, () => console.log('Server is running up on port 5000'))
